3d421b9116ed03523c113fea634128b08e988c2d
Abdullahil Azmi <abdullahilazmi@gmail.com>
2025-05-31 20:06:07 +0600

    Added the height card, will add conversion feature in the future.

--- lib/widgets/height_card.dart
+++ lib/widgets/height_card.dart
@@ -11,24 +11,47 @@ class _HeightCardState extends State<HeightCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-
       height: 140,
       width: 280,
 
-      decoration: BoxDecoration(
-        color: Color(0xff041838)
-      ),
-      
+      decoration: BoxDecoration(color: Color(0xff041838)),
+
       child: Center(
         child: Column(
           children: [
-            Text('HEIGHT', style: TextStyle(
-              color: Colors.white
-            ),)
+            const SizedBox(height: 10),
+
+            Text(
+              'HEIGHT',
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Montserrat',
+                fontSize: 20,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            Text(
+              'Enter your height below (in cm)',
+              style: TextStyle(color: Colors.white),
+            ),
+
+            const SizedBox(height: 2),
+
+            Container(
+              width: 70,
+
+              child: TextField(
+                cursorColor: Colors.white,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white), // ✅ Light text color here
+                decoration: InputDecoration(contentPadding: EdgeInsets.all(20)),
+              ),
+            ),
           ],
         ),
       ),
-
     );
   }
 }
